db52011fbf4f5829ed23278f8d49d124027b9466
popstas <popstas@gmail.com>
2015-11-23 00:44:49 +0500

    teamcity_ansible_report.sh, add changed tasks to summary

--- tests/teamcity_ansible_report.sh
+++ tests/teamcity_ansible_report.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
-last_log=$(find "%system.teamcity.build.tempDir%/ansible-run" \
+temp_dir="$1"
+
+last_log=$(find "$temp_dir/ansible-run" \
  -name "ansible-run-raw-RUNNER_*" -printf '%T@ %p\n' \
  | sort -n | tail -n1 | awk '{print $2}')
 
 summary=$(grep -o "ok=.*" "$last_log")
+changed_oneline=""
 
 get_value () {
-  echo "$summary" | grep -o "$1=[0-9]*" | cut -d'=' -f2
+	echo "$summary" | grep -o "$1=[0-9]*" | cut -d'=' -f2
+}
+
+get_by_state () {
+	state="$1"
+	if [ "$state" = "changed" ]; then
+		unfiltered=$(grep -B6 'changed": true' "$last_log" | grep "TASK\|changed" | cut -d'|' -f2-)
+
+		tasks=$(echo "$unfiltered" | awk 'NR % 2 == 1' \
+		| sed 's/TASK: \[//g' \
+		| sed 's/\] *.*//g')
+
+		msg=$(echo "$unfiltered" | awk 'NR % 2 == 0' \
+		| sed 's/.*"msg": "//g' \
+		| sed 's/",.*//g' \
+		| sed 's/\\n/|n/g')
+
+		IFS=$'\n'
+		for task in $tasks; do
+			tc_msg "message text='$task' status='WARNING'"
+			if [ -z "$changed_oneline" ]; then changed_oneline=", changed:"; fi
+			changed_oneline="$changed_oneline|n$task"
+		done
+	fi
 }
 
 tc_msg () {
-  echo "##teamcity[$@]"
+	echo "##teamcity[$@]"
 }
 
 ok=$(get_value ok)
@@ -26,6 +52,7 @@ fi
 
 if [ "$changed" != 0 ]; then
   tc_msg "message text='$changed changed' status='WARNING'"
+  get_by_state changed
   status="FAILURE"
 fi
 
@@ -34,4 +61,4 @@ if [ "$failed" != 0 ]; then
   status="FAILURE"
 fi
 
-tc_msg "buildStatus status='$status' text='$summary'"
+tc_msg "buildStatus status='$status' text='$summary|n$changed_oneline'"
