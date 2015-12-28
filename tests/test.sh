f7fbd29435ddbd6c337857b81821516822c6e6a2
popstas <popstas@gmail.com>
2015-12-28 14:51:06 +0500

    role site, test.sh fixes, site_templates

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PLAYBOOK="server.yml"
+PLAYBOOK=${ANSIBLE_PLAYBOOK:-"server.yml"}
+NO_LINT=${NO_LINT:-""}
+NO_IDEMPOTENCE=${NO_IDEMPOTENCE:-""}
 ARGS="--vault-password-file .vault_pass.txt -i build/hosts $@"
 
 set -e
-
-ansible-playbook $PLAYBOOK $ARGS
 
 exists (){
 	which $1 &> /dev/null
@@ -18,10 +18,17 @@ fi
 temp=$(mktemp -t ansible-test-XXXX)
 trap 'rm -f "$temp"' EXIT
 
-#ansible-lint $PLAYBOOK && \
-ansible-playbook $PLAYBOOK $ARGS --syntax-check && \
-ansible-playbook $PLAYBOOK $ARGS -vv && \
-ansible-playbook $PLAYBOOK $ARGS | tee $temp && \
-cat $temp | grep -q 'changed=0.*failed=0' \
+if [ -z "$NO_LINT" ]; then
+	ansible-lint $PLAYBOOK
+fi
+
+ansible-playbook $PLAYBOOK $ARGS --syntax-check
+
+ansible-playbook $PLAYBOOK $ARGS -vv
+
+if [ -z "$NO_IDEMPOTENCE" ]; then
+	ansible-playbook $PLAYBOOK $ARGS | tee $temp && \
+	cat $temp | grep -q 'changed=0.*failed=0' \
 	&& (echo 'Idempotence test: pass' && exit 0) \
 	|| (echo 'Idempotence test: fail' && exit 1)
+fi
